81d82b698c5bfa97868a19bb9c484a6af201e514
hugo.adamietz1 <hugo.adamietz1@600900B211DE412>
2018-02-22 11:51:46 +1100

    GoneHome game creatyed

---
+++ Assets/~GoneHome/Scripts/Death.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GoneHome
+{
+    public class Death : MonoBehaviour
+    {
+        void Died()
+        {
+
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.name == "DeathZone" ||
+                other.name == "Enemy")
+            {
+                Died();
+            }
+        }
+    }
+}
---
+++ Assets/~GoneHome/Scripts/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GoneHome
+{
+    [RequireComponent(typeof(Rigidbody))]
+
+    public class Player : MonoBehaviour
+    {
+        public float movementSpeed = 20f;
+
+        private Rigidbody rigid;
+        private Transform cam;
+        void Start()
+        {
+            rigid = GetComponent<Rigidbody>();
+
+            cam = Camera.main.transform;
+        }
+
+        void Update()
+        {
+            Movement();
+        }
+
+        void Movement()
+        {
+            float inputH = Input.GetAxis("Horizontal");
+            float inputV = Input.GetAxis("Vertical");
+
+            Vector3 inputDir = new Vector3(inputH, 0, inputV);
+
+            inputDir = Quaternion.AngleAxis(cam.eulerAngles.y, Vector3.up) * inputDir;
+
+            // Moves the position of the rigidbody without 
+            // effecting gravity
+            Vector3 position = transform.position;
+            position += inputDir * movementSpeed * Time.deltaTime;
+            rigid.MovePosition(position);
+        }
+    }
+}
